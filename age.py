045827d475bd9c6e676d4c0233485e0613a53d9a
RachelPun-SigmaLabs <trainee.hok-yan.pun@sigmalabs.co.uk>
2024-01-17 16:53:08 +0000

    edited age.py

--- age.py
+++ age.py
@@ -5,21 +5,21 @@ def date_val():
     while True:
         try:
             date = DT.datetime.strptime(
-                input("Please enter a date as yyyy/mm/dd"), "%Y/%m/%d")
+                input("Please enter a date as yyyy/mm/dd: "), "%Y/%m/%d")
             break
         except:
-            print("Not a valid date. Please try again")
+            print("Not a valid date. Please try again.")
     return date
 
 
 def age_cal():
-    years = today.year - date.year
+    age = today.year - date.year
     if today.month < date.month:
-        years -= 1
+        age -= 1
     elif today.month == date.month:
         if today.day < date.day:
-            years -= 1
-    return years
+            age -= 1
+    return age
 
 
 date = date_val()
